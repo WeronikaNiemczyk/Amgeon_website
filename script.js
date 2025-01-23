635dee17d019f87c0f880450dd1854e35ea030be
Weronika Niemczyk <112780270+WeronikaNiemczyk@users.noreply.github.com>
2025-01-23 15:02:51 +0100

    update slider, gallery, new files

--- script.js
+++ script.js
@@ -1,45 +1,5 @@
-// import lightGallery from "./node_modules/lightgallery";
 
-// Plugins;
-// import lgThumbnail from "lightgallery/plugins/thumbnail";
-// import lgZoom from "./node_modules/lightgallery/plugins/zoom";
 
-// Inicjalizacja LightGallery
-const galleryElement = document.getElementById("lightgallery");
-lightGallery(galleryElement, {
-  plugins: [lgZoom],
-  speed: 500,
-  controls: true, // Tylko strzałki nawigacji
-});
-
-// lightGallery(document.getElementById("lightgallery"), {
-//   plugins: [lgZoom, lgThumbnail],
-//   speed: 500,
-//   mode: "lg-fade",
-//   thumbnail: true,
-//   zoom: true,
-
-//   controls: true,
-//   download: true,
-//   allowMediaOverlap: true,
-//   closable: true,
-//   mobileSettings: {
-//     controls: false,
-//     showCloseIcon: false,
-//     download: false,
-//     rotate: false,
-//   },
-// });
-
-// const moreBtn = document.querySelector(".moreBtn");
-// const aboutUs = document.querySelector("#o-nas");
-
-// moreBtn.addEventListener("click", () => {
-//   aboutUs.scrollIntoView({
-//     behavior: "smooth",
-//     block: "start",
-//   });
-// });
 const moreBtn = document.querySelector(".moreBtn");
 const aboutUs = document.querySelector("#o-nas");
 
